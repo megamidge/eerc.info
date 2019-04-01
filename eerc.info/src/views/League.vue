7e9587ebd6ca2252c8479ed0794cfdbe6373ae92
Sam <samsmith@leisure-electronics.co.uk>
2019-04-01 19:00:13 +0000

    League.vue(under construction) + logos.

---
+++ eerc.info/src/views/League.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="margin-top:20%;width:100%;background: #18222c;">
+    <h1>{{$route.params.leagueCode}}</h1>
+    <h1>Under construction</h1>
+    <router-link to="/">
+      <h2>
+        <i>Oh fuck go back</i>
+      </h2>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+a {
+  color: #d1d1d1;
+  text-decoration: none;
+}
+a:visited {
+  color: unset;
+}
+a:hover {
+  color: #797979;
+}
+</style>
