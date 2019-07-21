1482091835d4d6343af17df197b8493c8025ac0a
Sam <samsmith@leisure-electronics.co.uk>
2019-07-21 23:40:08 +0100

    Gallery upload form suggests the password but its not a text field. FUCK knows why it wont autofill :angry:

--- public/api/gallery-upload.php
+++ public/api/gallery-upload.php
@@ -1,115 +1,4 @@
-<?php
-function greatestCommonDenominator($a, $b) {
-    if($b == 0) return $a;
-    return greatestCommonDenominator($b, $a % $b);
-}
-ini_set('display_errors', 1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
-if(isset($_POST["submit"])) {
-    if(!isset($_POST["password"]) && $_POST["password"] != "valterrihotass_pepefuck321"){
-        die("No or incorrect password.");
-    }
-    $target_dir = "../img/gallery/";
-    $target_file_name = uniqid('uploaded-',true) . '.' . strtolower(pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION));
-    $target_file = $target_dir . $target_file_name;
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-    //Check is actual image
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-    //Check file doesnt exist
-    if(file_exists($target_file)){
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-    //Check is supported file type
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif"){
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    //Upload
-    if($uploadOk == 0){
-        echo "File NOT uploaded.";
-        die();
-    } else {
-        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file)){
-            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . "has been uploaded.";
-        } else {
-            echo "Unknown error uploading file.";
-            die();
-        }
-    }
-    //get imgData as base64...
-    $img = file_get_contents($target_file);
-    list($width, $height) = getimagesize($target_file);
-    $gcd = greatestCommonDenominator($width, $height);
-    $arX = $width / $gcd;
-    $arY = $height / $gcd;
-    if($arX == $arY)
-    {
-        $arX = 24;
-        $arY = 24;
-    }
-    if($imageFileType == 'png')
-        $image = imagecreatefrompng($target_file);
-    else if($imageFileType == 'jpeg' || $imageFileType == 'jpg')
-        $image = imagecreatefromjpeg($target_file);
-    else if($imageFileType == 'gif')
-        $image = imagecreatefromgif($target_file);
-    $dst = imagecreatetruecolor($arX, $arY);
-    echo $arX . "   " . $arY;
-    imagecopyresampled($dst, $image, 0,0,0,0,$arX,$arY, $width, $height);
 
-
-    if($imageFileType == 'png')
-        imagepng($dst,'temp.png');
-    else if($imageFileType == 'jpeg' || $imageFileType == 'jpg')
-        imagejpeg($dst,'temp.'.$imageFileType);
-    else if($imageFileType == 'gif')
-        imagegif($dst,'temp.gif');
-    imagedestroy($dst);
-    imagedestroy($image);
-    $smallImage = file_get_contents('temp.'.$imageFileType);
-    $imgData = 'data:image/png;base64,'.base64_encode($smallImage);
-
-    //meta time, epic...
-    $metaFile = fopen($target_file . ".json","w");
-    $object = new stdClass();
-    $object->image = "/img/gallery/" . $target_file_name;
-    $object->name = $target_file_name;
-    if($_POST["description"] != "")
-        $object->description = $_POST['description'];
-    if($_POST["league"] != "")
-        $object->league = $_POST["league"];
-    if($_POST["season"] != ""){
-        if(strpos(strtolower($_POST["season"]),'round') == false)
-            $race = "Season" . $_POST["season"];
-        $object->season = $_POST["season"];
-    }
-    if($_POST["race"] != ""){
-        if(strpos(strtolower($_POST["race"]),'round') == false)
-            $race = "Round" . $_POST["race"];
-        $object->race = $_POST["race"];
-    }
-    if($_POST["track"] != "")
-        $object->track = $_POST["track"];
-    if($_POST["tags"] != ""){
-        $object->tags = explode(",", $_POST["tags"]);
-    }
-    $object->lowRes = $imgData;
-    $text = json_encode($object,JSON_UNESCAPED_SLASHES);
-    echo $text;
-    fwrite($metaFile, $text);
-    fclose($metaFile);
-}
-?>
 <style>
 body{
     --accent-dark: #12181f;
@@ -151,11 +40,154 @@ p{
     margin:0;
 }
 </style>
+<?php
+function greatestCommonDenominator($a, $b) {
+    if($b == 0) return $a;
+    return greatestCommonDenominator($b, $a % $b);
+}
+ini_set('display_startup_errors',true);
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+session_start();
+if(isset($_SESSION['messages']) && count($_SESSION['messages'])){
+    foreach($_SESSION['messages'] as &$message){
+        echo $message;
+    }
+}
+$_SESSION['messages'] = array();
+if(isset($_POST["submit"])) {
+    if(!isset($_POST["password"]) || $_POST["password"] != "valterrihotass_pepefuck321"){
+        array_push($_SESSION['messages'], "Incorrect password.<br/>");
+        header("Location:gallery-upload.php");
+        die("No or incorrect password.");
+    }
+    $target_dir = "../img/gallery/";
+    $target_file_name = uniqid('uploaded-',true) . '.' . strtolower(pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION));
+    $target_file = $target_dir . $target_file_name;
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    //Check is actual image
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        //echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        array_push($_SESSION['messages'], "File is not an image.<br/>");
+        echo "File is not an image.<br/>";
+        $uploadOk = 0;
+    }
+    //Check file doesnt exist
+    if(file_exists($target_file)){
+        array_push($_SESSION['messages'], "Sorry, file already exists.<br/>");
+        echo "Sorry, file already exists.<br/>";
+        $uploadOk = 0;
+    }
+    //Check is supported file type
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif"){
+        array_push($_SESSION['messages'], "Sorry, only <b>JPG, JPEG, PNG & GIF</b> files are allowed.<br/>");
+        echo "Sorry, only <b>JPG, JPEG, PNG & GIF</b> files are allowed.<br/>";
+        $uploadOk = 0;
+    }
+    //Upload
+    if($uploadOk == 0){
+        array_push($_SESSION['messages'], "File <b>NOT</b> uploaded.<br/>");
+        echo "File <b>NOT</b> uploaded.<br/>";
+        header("Location:gallery-upload.php");
+        die();
+    } else {
+        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file)){
+            array_push($_SESSION['messages'], "<b>The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.</b><br/>");
+            echo "<b>The file " . basename($_FILES["fileToUpload"]["name"]) . "has been uploaded.</b><br/>";
+        } else {
+            array_push($_SESSION['messages'], "Unknown error uploading file.<br/>");
+            echo "Unknown error uploading file.<br/>";
+
+            unlink($target_file);
+             header("Location:gallery-upload.php");
+            die();
+        }
+    }
+    //get imgData as base64...
+    $img = file_get_contents($target_file);
+    list($width, $height) = getimagesize($target_file);
+    $gcd = greatestCommonDenominator($width, $height);
+    $arX = $width / $gcd;
+    $arY = $height / $gcd;
+    if($arX == $arY)
+    {
+        $arX = 24;
+        $arY = 24;
+    }
+    if($imageFileType == 'png')
+        $image = imagecreatefrompng($target_file);
+    else if($imageFileType == 'jpeg' || $imageFileType == 'jpg')
+        $image = imagecreatefromjpeg($target_file);
+    else if($imageFileType == 'gif')
+        $image = imagecreatefromgif($target_file);
+    if(!$image){
+        unlink($target_file);
+
+        array_push($_SESSION['messages'], "Unhandled image error. Aborting. The uploaded image has been deleted.");
+        header("Location:gallery-upload.php");
+        die("Unhandled image error. Aborting.");
+    }
+    $dst = imagecreatetruecolor($arX, $arY);
+    echo $arX . "   " . $arY;
+    imagecopyresampled($dst, $image, 0,0,0,0,$arX,$arY, $width, $height);
+
+
+    if($imageFileType == 'png')
+        $res = imagepng($dst,'temp.png');
+    else if($imageFileType == 'jpeg' || $imageFileType == 'jpg')
+        $res = imagejpeg($dst,'temp.'.$imageFileType);
+    else if($imageFileType == 'gif')
+        $res = imagegif($dst,'temp.gif');
+    imagedestroy($dst);
+    imagedestroy($image);
+    if(!$res){
+        array_push($_SESSION['messages'], "Unkown image error. Image might have uploaded properly but something has failed. Shrug reacts only.");
+        header("Location:gallery-upload.php");
+        die("Unkown image error. Image might have uploaded properly but something has failed. Shrug reacts only.");
+    }
+    $smallImage = file_get_contents('temp.'.$imageFileType);
+    $imgData = 'data:image/png;base64,'.base64_encode($smallImage);
+
+    //meta time, epic...
+    $metaFile = fopen($target_file . ".json","w");
+    $object = new stdClass();
+    $object->image = "/img/gallery/" . $target_file_name;
+    $object->name = $target_file_name;
+    if($_POST["description"] != "")
+        $object->description = $_POST['description'];
+    if($_POST["league"] != "")
+        $object->league = $_POST["league"];
+    if($_POST["season"] != ""){
+        if(strpos(strtolower($_POST["season"]),'round') == false)
+            $race = "Season" . $_POST["season"];
+        $object->season = $_POST["season"];
+    }
+    if($_POST["race"] != ""){
+        if(strpos(strtolower($_POST["race"]),'round') == false)
+            $race = "Round" . $_POST["race"];
+        $object->race = $_POST["race"];
+    }
+    if($_POST["track"] != "")
+        $object->track = $_POST["track"];
+    if($_POST["tags"] != ""){
+        $object->tags = explode(",", $_POST["tags"]);
+    }
+    $object->lowRes = $imgData;
+    $text = json_encode($object,JSON_UNESCAPED_SLASHES);
+    fwrite($metaFile, $text);
+    fclose($metaFile);
+
+    header("Location:gallery-upload.php");
+}
+?>
+
 <form method="POST" enctype="multipart/form-data">
     <small><i>Admin page only. If you're not EERC staff, kindly fuck off back <a href="http://eerc.info">home</a>.</i></small>
     <h2>EERC Website Gallery Image Uploader</h2>
-    <p>Password:</p>
-    <input type="password" name="password" id="password" placeholder="password">
     <p>Image:</p>
     <input type="file" name="fileToUpload" id="fileToUpload">
     <h3>Meta data:</h3>
@@ -171,5 +203,7 @@ p{
     <input type="text" name="track" placeholder="InDiAnapOlIs MoToR SPEeDwaY">
     <p>Extra tags (comma seperated):</p>
     <input type="text" name="tags" placeholder="Ooh, ee, ooh, ooh, ah, ting, tag, wallaballa, bing, bang">
+    <p>Password:</p>
+    <input type="text" name="password" id="password" placeholder="password" autocomplete="password">
     <input type="submit" value="Upload Image" name="submit" style="border: solid 0.1rem var(--accent-bright);background-color:var(--accent-bright-alt);">
 </form>
