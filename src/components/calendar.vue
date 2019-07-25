da448e487dd4f0eb58b33f45da300491e0977752
Sam <samsmith@leisure-electronics.co.uk>
2019-07-25 22:03:50 +0100

    Update calendar, improve calendar to show time + last month blank spots

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,28 +1,37 @@
 <template>
 	<div class="calendar">
 		<div class="calendar-header">
-			<chevron-left style="cursor:pointer;" @click="subtractMonth"></chevron-left>
+			<chevron-left :size="48" class="nav-icon" style="cursor:pointer;" @click="subtractMonth"></chevron-left>
 			<h4>{{month + ' - ' + year}}</h4>
-			<chevon-right style="cursor:pointer;" @click="addMonth"></chevon-right>
+			<chevon-right :size="48" class="nav-icon" style="cursor:pointer;" @click="addMonth"></chevon-right>
 		</div>
-		<ul class="weekdays">
-			<li v-for="day in days" :key="`day${day}`">{{day}}</li>
-		</ul>
+		<!-- <ul class="weekdays"></ul> -->
 		<ul class="dates">
-			<li class="blank" v-for="(blank,date) in firstDayOfMonth" :key="`blank${date}`">
-				<!-- <span class="date">
+			<li class="weekday" v-for="day in days" :key="`day${day}`">{{day}}</li>
+			<li
+				class="blank"
+				:class="{'current-day': date == initialDate && month == initialMonth && year == initialYear}"
+				v-for="(blank,date) in firstDayOfMonth"
+				:key="`blank${date}`"
+			>
+				<span class="date">
 					<p>{{lastMonthDay(date)}}</p>
 					<div
-						v-for="(event,index) in eventsOnDate(new Date(`${year}-${lastMonthInt}-${dayInt((date + 1) + daysInLastMonth - (date))}`))"
+						v-for="(event,index) in eventsOnDate(new Date(`${year}-${lastMonthInt}-${dayInt(lastMonthDay(date))}`))"
 						:key="`event${index}`"
 						class="event"
 						:style="{backgroundColor: leagueColour(event.league)}"
 						@click="$router.push(`/league/${event.league}`)"
 					>
-						{{event.league}}
-						<div class="event-info">{{event.event.track}}</div>
+						<div class="info-row">
+							{{event.league}}
+							<div class="event-info">{{event.event.track || event.event.location}}</div>
+						</div>
+						<div class="info-row">
+							<div class="event-time">{{getTime(event.event.utcDateTime)}}</div>
+						</div>
 					</div>
-				</span>-->
+				</span>
 				&nbsp;
 			</li>
 			<li
@@ -39,15 +48,22 @@
 						:style="{backgroundColor: leagueColour(event.league)}"
 						@click="$router.push(`/league/${event.league}`)"
 					>
-						{{event.league}}
-						<div class="event-info">{{event.event.track}}</div>
+						<div class="info-row">
+							{{event.league}}
+							<div class="event-info">{{event.event.track || event.event.location}}</div>
+						</div>
+						<div class="info-row">
+							<div class="event-time">{{getTime(event.event.utcDateTime)}}</div>
+						</div>
 					</div>
 				</span>
+				&nbsp;
 			</li>
 			<li
 				class="blank"
 				v-for="(blank,date) in (42-firstDayOfMonth-daysInMonth)"
 				:key="`blank2${date}`"
+				:class="{'current-day': date == initialDate && month == initialMonth && year == initialYear}"
 			>
 				<span class="date">
 					<p>{{date+1}}</p>
@@ -58,10 +74,16 @@
 						:style="{backgroundColor: leagueColour(event.league)}"
 						@click="$router.push(`/league/${event.league}`)"
 					>
-						{{event.league}}
-						<div class="event-info">{{event.event.track}}</div>
+						<div class="info-row">
+							{{event.league}}
+							<div class="event-info">{{event.event.track || event.event.location}}</div>
+						</div>
+						<div class="info-row">
+							<div class="event-time">{{getTime(event.event.utcDateTime)}}</div>
+						</div>
 					</div>
 				</span>
+				&nbsp;
 			</li>
 			<!-- <li v-for="(blank, index) in firstDayOfMonth" :key="`blank2${index}`">&nbsp;</li> -->
 		</ul>
@@ -127,7 +149,7 @@ export default {
 		},
 		daysInLastMonth() {
 			let d = moment(this.dateContext)
-			d.subtract(1)
+			d.subtract(1, 'month')
 			return d.daysInMonth()
 		},
 	},
@@ -160,7 +182,14 @@ export default {
 			this.dateContext = moment(this.dateContext).subtract(1, 'month')
 		},
 		lastMonthDay(date) {
-			return this.daysInLastMonth
+			return this.daysInLastMonth - this.firstDayOfMonth + 1 + date
+		},
+		getTime(utcDateString) {
+			let date = new Date(utcDateString)
+			return `${this.padNumber(date.getHours())}:${this.padNumber(date.getMinutes())}`
+		},
+		padNumber(number) {
+			return ('0' + number).slice(-2)
 		},
 	},
 }
@@ -190,15 +219,11 @@ export default {
 	grid-gap: 0.4rem;
 	margin: 0.2rem;
 }
-.weekdays li {
-	background: var(--accent-bright);
-	padding: 0.4rem;
-}
 .dates {
 	display: grid;
 	grid-template-columns: repeat(7, 1fr);
 
-	grid-auto-rows: 1fr;
+	grid-auto-rows: auto 1fr;
 	grid-gap: 0.4rem;
 	margin: 0.2rem;
 }
@@ -206,6 +231,16 @@ export default {
 	background: var(--accent-dark);
 	width: 100%;
 	position: relative;
+}
+.dates .current-day {
+	background: var(--accent-bright);
+	border-radius: 0.2rem;
+}
+.dates .weekday {
+	background: var(--accent-bright);
+	/* padding: 0.4rem; */
+	padding-top: 0.2rem;
+	padding-bottom: 0.2rem;
 }
 .date {
 	padding: 0.1rem;
@@ -232,16 +267,27 @@ export default {
 	border-radius: 0.2rem;
 	font-size: 0.9rem;
 	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
+	flex-direction: column;
+	justify-content: flex-start;
 	padding: 0.1rem;
 	cursor: pointer;
 	color: var(--accent-dark);
 	font-weight: bold;
 }
-.event .event-info {
+.event .info-row {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
+.event .info-row .event-info {
 	display: block;
 	text-align: right;
+}
+.event .info-row .event-time {
+	display: block;
+	text-align: right;
+	flex-grow: 1;
+	font-weight: normal;
 }
 .event:hover {
 	background: var(--accent-bright-alt);
@@ -251,13 +297,27 @@ ul {
 	list-style: none;
 	padding: 0;
 }
+
+.material-design-icon.nav-icon {
+	height: 2rem;
+	width: 2rem;
+}
+.material-design-icon.nav-icon > .material-design-icon__svg {
+	height: 2rem;
+	width: 2rem;
+}
 @media screen and (max-width: 800px) {
-	.event .event-info {
+	.event .info-row .event-info {
+		display: none;
+	}
+}
+@media screen and (max-width: 600px) {
+	.event .info-row .event-time {
 		display: none;
 	}
 }
 @media screen and (max-width: 360px) {
-	.weekdays li {
+	.dates .weekday {
 		font-size: 0.6rem;
 		padding: 0.1rem;
 	}
