63f5cf574b7a2ad44ca89b63d77de6d8f6166654
ss.samsmith98 <ss.samsmith98@outlook.com>
2020-07-13 17:38:46 +0000

    Added season editor, started added event editor. Both need work.
    Event editor still requires proper addition, as well as session editor.
    Nothing is saved to databse yet (this may be non-trivial, too... will need to collect up all the data back out of the tables)
    Moved the specific tables into their own components to make things MUCH easier.

--- eerc.info/src/pages/Leagues/League.vue
+++ eerc.info/src/pages/Leagues/League.vue
@@ -1,100 +1,49 @@
 <template>
-  <q-page class="flex column items-center" v-if="league">
-    <q-toolbar class="bg-secondary">
-      <q-btn flat round dense icon="arrow_back" @click="$router.push('/leagues')"/>
-      <q-toolbar-title>
-        <editable-text v-model="titleInput" @submit="submitNewTitle" :loading="loading" class="text-h6"/>
-      </q-toolbar-title>
-      <q-badge class="text-caption" color="dark">{{ league.id }}</q-badge>
-    </q-toolbar>
-    <h1>{{ league.id }}</h1>
-    <p>{{ getLeague('DIRT') }}</p>
-    <q-table title="League Settings"
-      :data="leagueSettings.data"
-      :columns="leagueSettings.columns"
-      :pagination="leagueSettings.initialPagination"
-      style="max-width:90%"
-      class="q-px-md q-py-sm q-table--wrap" hide-pagination
-      dense>
-      <template v-slot:body="props">
-        <q-tr :props="props">
-          <q-td key="Key" :props="props" class="q-pa-none">
-            {{ props.row.key }}
-          </q-td>
-          <q-td key="Value" :props="props">
-            {{ props.row.value}}
-          </q-td>
-          <q-td key="Actions" :props="props" class="q-pa-none" style="width:10rem">
-            <q-btn color="secondary" icon="edit" @click="editItem(props.row)" no-caps/>
-            <q-btn color="accent" icon="delete" @click="deleteItem(props.row)" no-caps class="q-ml-xs"/>
-          </q-td>
-        </q-tr>
-      </template>
-      <template v-slot:bottom>
-      <div style="width: 100%" class="flex row items-center justify-end">
-        <q-btn label="Add" icon-right="add" class="bg-primary" @click="showDialog = true; editing = false"/>
-        <q-btn label="Save" icon-right="save" class="bg-primary q-ml-sm" disabled/>
-
-        <q-dialog v-model="showDialog">
-          <q-card>
-            <q-card-section class="q-pb-none">
-              <h6 class="q-ma-none">{{ editing ? 'Edit Item' : 'Add Item'}}</h6>
-            </q-card-section>
-            <q-card-section class="q-pt-none">
-              <div class="column">
-                <q-input v-model="editedItem.key" label="Key"/>
-                <q-input v-model="editedItem.value" label="Value" type="textarea"/>
-              </div>
-            </q-card-section>
-            <q-card-actions class="q-pt-none" align="right">
-              <q-btn :label="editing ? 'Edit' : 'Add'" v-close-popup @click="addRow"/>
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-        <q-dialog v-model="showDeleteDialog">
-          <q-card>
-            <!-- <q-card-section class="q-pb-none">
-              <h6 class="q-ma-none">Are you sure?</h6>
-            </q-card-section> -->
-            <q-card-section>
-              <q-img src="https://media1.tenor.com/images/546784721fde6b86621a8b541e48b8c3/tenor.gif?itemid=14258954"
-              style="min-width:20em;"/>
-              <p class="q-ma-none">About to delete key value pair:</p>
-              <h6 class="q-ma-none text-italic text-right" v-if="leagueSettings.data && leagueSettings.data[deleteIndex]">{{ leagueSettings.data[deleteIndex].key }}: {{ leagueSettings.data[deleteIndex].value }}</h6>
-            </q-card-section>
-            <q-card-actions class="q-pt-none" align="right">
-              <q-btn label="Cancel" v-close-popup @click="close"/>
-              <q-btn label="Confirm" v-close-popup @click="confirmDeleteItem"/>
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
-      </template>
-    </q-table>
-  </q-page>
-  <q-page v-else class="flex items-center justify-center">
-    <q-spinner size="10em"/>
-  </q-page>
+  <q-layout view="hHh lpR fFf">
+    <template v-if="league">
+      <q-header>
+        <q-toolbar class="bg-secondary">
+            <q-btn flat round dense icon="arrow_back" @click="$router.push('/leagues')"/>
+            <q-toolbar-title>
+              <editable-text v-model="titleInput" @submit="submitNewTitle" :loading="loading" class="text-h6"/>
+            </q-toolbar-title>
+            <q-badge class="text-caption" color="dark">{{ league.id }}</q-badge>
+          </q-toolbar>
+      </q-header>
+      <q-page-container>
+        <q-page class="flex column items-center q-gutter-md" v-if="league">
+          <h1>{{ league.id }}</h1>
+          <p>{{ getLeague('DIRT') }}</p>
+          <settings :league="league"/>
+          <seasons :seasons="league.seasons"/>
+        </q-page>
+    </q-page-container>
+  </template>
+  <template v-else>
+    <q-page-container>
+      <q-page class="flex items-center justify-center">
+        <q-spinner size="10em"/>
+      </q-page>
+    </q-page-container>
+  </template>
+  </q-layout>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import EditableText from 'components/EditableText'
+import Settings from 'components/Leagues/Settings'
+import Seasons from 'components/Leagues/Seasons'
 export default {
   components: {
-    EditableText
+    EditableText,
+    Settings,
+    Seasons
   },
   data () {
     return {
       titleInput: '',
-      loading: false,
-      leagueSettings: { },
-      showDialog: false,
-      editedIndex: -1,
-      editedItem: { key: '', value: '' },
-      editing: false,
-      showDeleteDialog: false,
-      deleteIndex: -1
+      loading: false
     }
   },
   computed: {
@@ -106,38 +55,6 @@ export default {
     }
   },
   methods: {
-    addRow () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.leagueSettings.data[this.editedIndex], this.editedItem)
-      } else {
-        this.leagueSettings.data.push(this.editedItem)
-      }
-      this.close()
-    },
-    editItem (item) {
-      this.editing = true
-      this.editedIndex = this.leagueSettings.data.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.showDialog = true
-    },
-    deleteItem (item) {
-      this.deleteIndex = this.leagueSettings.data.indexOf(item)
-      this.showDeleteDialog = true
-    },
-    confirmDeleteItem () {
-      this.leagueSettings.data.splice(this.deleteIndex, 1)
-      this.close()
-    },
-    close () {
-      this.showDialog = false
-      this.showDeleteDialog = false
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, { key: '', value: '' })
-        this.editedIndex = -1
-        this.deleteIndex = -1
-        this.editing = false
-      }, 300)
-    },
     submitNewTitle () {
       // this.titleEdit = false
       if (this.titleInput === this.league.name)
@@ -151,32 +68,8 @@ export default {
           this.loading = false
         })
     },
-    createLeagueSettings () {
-      const newLeagueSettings = {
-        initialPagination: {
-          rowsPerPage: 0
-        },
-        columns: [
-          { name: 'Key', required: true, label: 'Key', field: row => row.key, align: 'left' },
-          { name: 'Value', required: true, label: 'Value', field: row => row.value, align: 'left' },
-          { name: 'Actions', label: 'Actions', field: 'actions' }
-        ],
-        data: [
-
-        ]
-      }
-      Object.keys(this.league).forEach(key => {
-        if (!Array.isArray(this.league[key]))
-          newLeagueSettings.data.push({
-            key: key,
-            value: this.league[key]
-          })
-      })
-      this.leagueSettings = newLeagueSettings
-    },
     init () {
       this.titleInput = this.league.name
-      this.createLeagueSettings()
     }
   },
   mounted () {
