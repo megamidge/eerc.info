2e9de4da72f0b78b8ceb13e8cee1ec8969d3876a
Sam Smith <ss.samsmith98@outlook.com>
2021-05-17 17:20:29 +0100

    Fixed result change checking when publishing just setting every result as a change.

--- eerc.info/admin/src/store/season/actions.js
+++ eerc.info/admin/src/store/season/actions.js
@@ -1,38 +1,39 @@
 import { firestoreAction } from 'vuexfire'
-import {
-  leagueSeasonEventsRef,
-  publishSeasonChanges as fsPublishSeasonChanges
-} from 'src/services/firebase/firestore'
+import { leagueSeasonEventsRef, publishSeasonChanges as fsPublishSeasonChanges } from 'src/services/firebase/firestore'
 /*
 export function someAction (context) {
 }
 */
 
-export function setupSeason ({ dispatch, getters, commit }, { leagueId, season, sync }) {
+export function setupSeason({ dispatch, getters, commit }, { leagueId, season, sync }) {
   commit('setSync', sync)
   dispatch('getSeasonEvents', { leagueId, seasonId: season.id, sync })
-  this.watch(() => getters.seasonEvents, (events) => eventWatcher(events, leagueId, season.id, this, dispatch, sync))
+  this.watch(
+    () => getters.seasonEvents,
+    (events) => eventWatcher(events, leagueId, season.id, this, dispatch, sync)
+  )
 }
 
 export const getSeasonEvents = firestoreAction(({ bindFirestoreRef, commit }, { leagueId, seasonId, sync }) => {
-  if (sync)
-    return bindFirestoreRef('events', leagueSeasonEventsRef(leagueId, seasonId))
+  if (sync) return bindFirestoreRef('events', leagueSeasonEventsRef(leagueId, seasonId))
   else {
-    leagueSeasonEventsRef(leagueId, seasonId).get().then(snap => {
-      const data = snap.docs.map(doc => {
-        var docData = { ...doc.data() }
-        Object.defineProperty(docData, 'id', { value: doc.id, enumerable: false })
-        return docData
+    leagueSeasonEventsRef(leagueId, seasonId)
+      .get()
+      .then((snap) => {
+        const data = snap.docs.map((doc) => {
+          var docData = { ...doc.data() }
+          Object.defineProperty(docData, 'id', { value: doc.id, enumerable: false })
+          return docData
+        })
+        commit('setEvents', data)
       })
-      commit('setEvents', data)
-    })
   }
 })
 
 import eventModule from '../event'
-function eventWatcher (events, leagueId, seasonId, context, dispatch, sync) {
+function eventWatcher(events, leagueId, seasonId, context, dispatch, sync) {
   var leagueModuleName = sync ? leagueId : `edit_${leagueId}`
-  events.forEach(event => {
+  events.forEach((event) => {
     if (!context.hasModule([leagueModuleName, seasonId, event.id])) {
       context.registerModule([leagueModuleName, seasonId, event.id], eventModule)
       dispatch(`${event.id}/setupEvent`, { leagueId, seasonId, event, sync })
@@ -40,13 +41,13 @@ function eventWatcher (events, leagueId, seasonId, context, dispatch, sync) {
   })
 }
 
-export function reset ({ dispatch, commit }, { leagueId, seasonId, sync }) {
+export function reset({ dispatch, commit }, { leagueId, seasonId, sync }) {
   commit('clearDeletedEvents')
   dispatch('getSeasonEvents', { leagueId, seasonId, sync })
 }
 
 import deepEqual from 'deep-equal'
-export function publishSeasonChanges (context, { leagueId, seasonId }) {
+export function publishSeasonChanges(context, { leagueId, seasonId }) {
   const changes = [] // any items that are different to the synced/live version of the data are pushed into this array in {path,id,data} format.
   // if the change is a DELETE, data should be undefined or null or an empty object.
   // if the ID is undefined, firebase should create one for us.
@@ -60,7 +61,7 @@ export function publishSeasonChanges (context, { leagueId, seasonId }) {
     })
   }
   const deletedEvents = context.getters.eventsDeleted
-  deletedEvents.forEach(event => {
+  deletedEvents.forEach((event) => {
     changes.push({
       path: `leagues/${leagueId}/seasons/${seasonId}/events`,
       id: event.value.id,
@@ -68,7 +69,7 @@ export function publishSeasonChanges (context, { leagueId, seasonId }) {
     })
   })
   const events = context.getters.seasonEvents
-  events.forEach(event => {
+  events.forEach((event) => {
     // console.log('event', event.id, event)
     const eventGet = context.rootGetters[`${leagueId}/${seasonId}/event`]
     if (!deepEqual(event, eventGet ? eventGet(event.id) : {})) {
@@ -81,14 +82,14 @@ export function publishSeasonChanges (context, { leagueId, seasonId }) {
     const collectionName = event.type === 'rally' ? 'stages' : 'sessions'
     const sessions = context.getters[`${event.id}/sessions`]
     const deletedSessions = context.getters[`${event.id}/sessionsDeleted`]
-    deletedSessions.forEach(session => {
+    deletedSessions.forEach((session) => {
       changes.push({
         path: `leagues/${leagueId}/seasons/${seasonId}/events/${event.id}/${collectionName}`,
         id: session.value.id,
         data: null
       })
     })
-    sessions.forEach(session => {
+    sessions.forEach((session) => {
       // console.log('session', session.id, session)
       const seshGet = context.rootGetters[`${leagueId}/${seasonId}/${event.id}/session`]
       if (!deepEqual(session, seshGet ? seshGet(session.id) : {})) {
@@ -104,11 +105,11 @@ export function publishSeasonChanges (context, { leagueId, seasonId }) {
       // find all the results that are in `liveResults` but not in `results`.
       if (liveResults) {
         // this only needs doing if there are actually any liveResults for this session :)
-        const difference = liveResults.filter(lr => {
-          return !results.find(r => r.id === lr.id)
+        const difference = liveResults.filter((lr) => {
+          return !results.find((r) => r.id === lr.id)
         })
         // push those results as delete changes...
-        difference.forEach(result => {
+        difference.forEach((result) => {
           changes.push({
             path: `leagues/${leagueId}/seasons/${seasonId}/events/${event.id}/${collectionName}/${session.id}/results`,
             id: result.id,
@@ -116,11 +117,11 @@ export function publishSeasonChanges (context, { leagueId, seasonId }) {
           })
         })
       }
-      results.forEach(result => {
+      results.forEach((result) => {
         // console.log('result', result.id, result)
-        const resultGet = context.rootGetters[`${leagueId}/${seasonId}/${event.id}/${session.id}/result`]
-        if (!deepEqual(result, resultGet ? (result.id) : {})) {
-          const { id, ...resultData } = result
+        const storedResult = context.rootGetters[`${leagueId}/${seasonId}/${event.id}/${session.id}/result`](result.id)
+        const { id, ...resultData } = result
+        if (!deepEqual(resultData, storedResult)) {
           changes.push({
             path: `leagues/${leagueId}/seasons/${seasonId}/events/${event.id}/${collectionName}/${session.id}/results`,
             id: result.id,
