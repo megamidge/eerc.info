2fba89c829adcbb9c0fbb896b940d1cff996a925
Sam Smith <ss.samsmith98@outlook.com>
2022-06-05 11:52:19 +0100

    Set stuff up for a basic release of the 'beta' (new) site

--- eerc.info/site/src/components/leagues/LeagueSeason.vue
+++ eerc.info/site/src/components/leagues/LeagueSeason.vue
@@ -1,25 +1,33 @@
 <template>
-<q-card>
+  <q-card>
     <q-card-section class="row justify-between bg-secondary q-py-sm">
-        <div class="row no-wrap items-center">
-            <p class="q-ma-none text-subtitle1 text-uppercase">Class:</p>
-            <p class="q-ma-none q-ml-sm text-subtitle1 text-bold">{{season.vehicleClass}}</p>
-        </div>
+      <div class="row no-wrap items-center">
+        <p class="q-ma-none text-subtitle1 text-uppercase">Class:</p>
+        <p class="q-ma-none q-ml-sm text-subtitle1 text-bold">{{ season.vehicleClass }}</p>
+      </div>
     </q-card-section>
     <q-card-section class="bg-primary">
-        Perhaps if the season has finished, the overall result can be here.
+      <!-- Perhaps if the season has finished, the overall result can be here.
         For example, show the top 3, and a link to 'view full results'.
         This could either open a page or a dialog (either way, it would be ideal to be able to link directly to it).
-        This would show the full season results (names/teams in order of classification, points accrued. Any other info i guess.)
-
+        This would show the full season results (names/teams in order of classification, points accrued. Any other info i guess.) -->
     </q-card-section>
     <q-card-section class="row wrap">
-      <season-event v-for="(event,index) in events" :key="event.id" :index="index+1" :event="event" :leagueId="leagueId" :seasonId="season.id" class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4"/>
+      <season-event
+        v-for="(event, index) in events"
+        :key="event.id"
+        :index="index + 1"
+        :event="event"
+        :leagueId="leagueId"
+        :seasonId="season.id"
+        class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4"
+      />
     </q-card-section>
-</q-card>
+  </q-card>
 </template>
 
 <script>
+/* eslint-disable */
 import SeasonEvent from './seasons/SeasonEvent.vue'
 export default {
   components: { SeasonEvent },
@@ -34,13 +42,11 @@ export default {
     }
   },
   computed: {
-    events () {
+    events() {
       return this.$store.getters[`${this.leagueId}/${this.season.id}/seasonEvents`]
     }
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- eerc.info/site/src/layouts/MainLayout.vue
+++ eerc.info/site/src/layouts/MainLayout.vue
@@ -2,7 +2,13 @@
   <q-layout view="lHh Lpr lff">
     <q-header elevated reveal>
       <q-toolbar v-if="!$q.platform.is.mobile && !$q.screen.xs">
-        <q-img src="/logos/logo_dark.svg" :ratio="1 / 1" class="logo q-ma-md" style="cursor:pointer;" @click="$router.push('/')"/>
+        <q-img
+          src="/logos/logo_dark.svg"
+          :ratio="1 / 1"
+          class="logo q-ma-md"
+          style="cursor:pointer;"
+          @click="$router.push('/')"
+        />
         <q-btn
           v-for="headerLink in headerLinks"
           :key="headerLink.label"
@@ -11,14 +17,31 @@
           class="text-h6"
           :to="headerLink.link"
         />
-        <q-space/>
-        <q-img src="/logos/grid-finder-partner-logo.png" :ratio="1 / 1" class="logo q-ma-md" style="cursor:pointer;" @click="openURL(socials.gridFinder)"/>
-        <q-btn icon="mdi-discord" @click="openURL(socials.discord)" flat :label="!$q.screen.sm ? 'Join our Discord!' : ''"/>
+        <q-space />
+        <q-img
+          src="/logos/grid-finder-partner-logo.png"
+          :ratio="1 / 1"
+          class="logo q-ma-md"
+          style="cursor:pointer;"
+          @click="openURL(socials.gridFinder)"
+        />
+        <q-btn
+          icon="mdi-discord"
+          @click="openURL(socials.discord)"
+          flat
+          :label="!$q.screen.sm ? 'Join our Discord!' : ''"
+        />
       </q-toolbar>
       <q-toolbar elevated v-else>
-        <q-img src="/logos/logo_dark.svg" :ratio="1 / 1" class="logo mobile" style="cursor:pointer;" @click="$router.push('/')"/>
-        <q-space/>
-        <q-btn icon="mdi-menu" @click="drawer = true" flat padding="0"/>
+        <q-img
+          src="/logos/logo_dark.svg"
+          :ratio="1 / 1"
+          class="logo mobile"
+          style="cursor:pointer;"
+          @click="$router.push('/')"
+        />
+        <q-space />
+        <q-btn icon="mdi-menu" @click="drawer = true" flat padding="0" />
       </q-toolbar>
     </q-header>
 
@@ -26,7 +49,13 @@
       <router-view />
     </q-page-container>
 
-    <q-drawer v-model="drawer" v-if="$q.platform.is.mobile || $q.screen.xs" side="right" elevated content-class="column items-stretch justify-center q-pa-md bg-primary">
+    <q-drawer
+      v-model="drawer"
+      v-if="$q.platform.is.mobile || $q.screen.xs"
+      side="right"
+      elevated
+      content-class="column items-stretch justify-center q-pa-md bg-primary"
+    >
       <q-list separator class="self-stretch justify-center full-height column">
         <q-item
           v-for="headerLink in headerLinks"
@@ -40,37 +69,38 @@
             {{ headerLink.label }}
           </q-item-label>
         </q-item>
-        <q-space/>
-        <q-img src="/logos/grid-finder-partner-logo.png" :ratio="1 / 1" class="logo q-ma-md" style="cursor:pointer;"/>
-        <q-item class="text-h4 text-bold text-left"
-        clickable
-        @click="openURL(socials.discord)">
+        <q-space />
+        <q-img src="/logos/grid-finder-partner-logo.png" :ratio="1 / 1" class="logo q-ma-md" style="cursor:pointer;" />
+        <q-item class="text-h4 text-bold text-left" clickable @click="openURL(socials.discord)">
           <q-item-section>
             <q-item-label>
               Discord
             </q-item-label>
           </q-item-section>
           <q-item-section avatar>
-            <q-icon name="mdi-discord" size="2.4rem"/>
+            <q-icon name="mdi-discord" size="2.4rem" />
           </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
 
     <q-footer>
-      <q-toolbar :class="{ 'column': $q.platform.is.mobile || $q.screen.xs}">
+      <q-toolbar :class="{ column: $q.platform.is.mobile || $q.screen.xs }">
         <q-toolbar-title>
           <p class="text-caption q-ma-none">European Electronic Racing Community</p>
         </q-toolbar-title>
         <div>
-          <q-btn icon="mdi-discord" @click="openURL(socials.discord)" flat/>
-          <q-btn icon="mdi-twitter" @click="openURL(socials.twitter)" flat/>
-          <q-btn icon="mdi-instagram" @click="openURL(socials.instagram)" flat/>
-          <q-btn icon="mdi-youtube" @click="openURL(socials.youtube)" flat/>
+          <q-btn icon="mdi-discord" @click="openURL(socials.discord)" flat />
+          <q-btn icon="mdi-twitter" @click="openURL(socials.twitter)" flat />
+          <q-btn icon="mdi-instagram" @click="openURL(socials.instagram)" flat />
+          <q-btn icon="mdi-youtube" @click="openURL(socials.youtube)" flat />
         </div>
       </q-toolbar>
       <q-toolbar class="row fill">
-        <div class="row items-center full-width" :class="$q.platform.is.mobile || $q.screen.xs ? 'justify-center text-center' : 'justify-between'">
+        <div
+          class="row items-center full-width"
+          :class="$q.platform.is.mobile || $q.screen.xs ? 'justify-center text-center' : 'justify-between'"
+        >
           <div class="column q-py-md">
             <small>Help fund EERC, donate with PayPal.</small>
             <small>Donations ensure EERC can continue to serve it's community and host events.</small>
@@ -78,27 +108,38 @@
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="top" class="q-py-md">
             <input type="hidden" name="cmd" value="_s-xclick" />
             <input type="hidden" name="hosted_button_id" value="WT96RWAPDAZ8S" />
-            <q-btn label="Donate" type="submit" icon="mdi-charity" color="accent" class="text-bold text-h6" padding="sm"/>
+            <q-btn
+              label="Donate"
+              type="submit"
+              icon="mdi-charity"
+              color="accent"
+              class="text-bold text-h6"
+              padding="sm"
+            />
           </form>
         </div>
       </q-toolbar>
       <q-toolbar class="row fill justify-center">
-        <q-space v-if="!$q.platform.is.mobile && !$q.screen.xs"/>
-        <q-icon name="img:/icons/flag/1.png" class="q-mr-sm"/>
-        <div class="flagsAttribute text-italic text-caption">Icons made by <a class="text-bold" target="_blank" href="https://www.freepik.com" title="Freepik">Freepik</a> from <a class="text-bold" target="_blank" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        <q-space v-if="!$q.platform.is.mobile && !$q.screen.xs" />
+        <q-icon name="img:/icons/flag/1.png" class="q-mr-sm" />
+        <div class="flagsAttribute text-italic text-caption">
+          Icons made by
+          <a class="text-bold" target="_blank" href="https://www.freepik.com" title="Freepik">Freepik</a> from
+          <a class="text-bold" target="_blank" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+        </div>
       </q-toolbar>
       <q-toolbar>
         {{ $q.screen.name }}
       </q-toolbar>
       <q-toolbar class="bg-black">
-        <q-icon color="primary" name="mdi-circle"/>
-        <q-icon color="secondary" name="mdi-circle"/>
-        <q-icon color="accent" name="mdi-circle"/>
-        <q-icon color="dark" name="mdi-circle"/>
-        <q-icon color="positive" name="mdi-circle"/>
-        <q-icon color="negative" name="mdi-circle"/>
-        <q-icon color="info" name="mdi-circle"/>
-        <q-icon color="warning" name="mdi-circle"/>
+        <q-icon color="primary" name="mdi-circle" />
+        <q-icon color="secondary" name="mdi-circle" />
+        <q-icon color="accent" name="mdi-circle" />
+        <q-icon color="dark" name="mdi-circle" />
+        <q-icon color="positive" name="mdi-circle" />
+        <q-icon color="negative" name="mdi-circle" />
+        <q-icon color="info" name="mdi-circle" />
+        <q-icon color="warning" name="mdi-circle" />
       </q-toolbar>
     </q-footer>
   </q-layout>
@@ -108,7 +149,8 @@
 import { openURL } from 'quasar'
 export default {
   name: 'MainLayout',
-  data () {
+  // eslint-disable-next-line
+  data() {
     return {
       drawer: false,
       openURL: openURL,
@@ -127,15 +169,15 @@ export default {
         {
           label: 'Leagues',
           link: '/Leagues'
-        },
-        {
-          label: 'Gallery',
-          link: '/Gallery'
-        },
-        {
-          label: 'About',
-          link: '/About'
         }
+        // {
+        //   label: 'Gallery',
+        //   link: '/Gallery'
+        // },
+        // {
+        //   label: 'About',
+        //   link: '/About'
+        // }
       ]
     }
   }
@@ -145,34 +187,34 @@ export default {
 .logo {
   max-height: 5rem;
   max-width: 5rem;
-  object-fit:contain;
+  object-fit: contain;
   &.mobile {
     max-height: 2rem;
-    max-width:2rem;
+    max-width: 2rem;
   }
 }
 
 @media screen and (min-width: $breakpoint-lg-min) {
-    %max-width {
-      margin: 0 auto;
-      max-width: $breakpoint-lg;
-    }
-    .q-page{
+  %max-width {
+    margin: 0 auto;
+    max-width: $breakpoint-lg;
+  }
+  .q-page {
+    @extend %max-width;
+  }
+  .q-header,
+  .q-footer {
+    .q-toolbar {
       @extend %max-width;
     }
-    .q-header, .q-footer{
-      .q-toolbar {
-        @extend %max-width;
-      }
-    }
+  }
 }
 
 .page-container {
-  background: linear-gradient(rgba(18,18,18, 0.65), rgba(18,18,18, 0.65)),
-    url('/images/background/eerc_background.png') no-repeat fixed,
-  rgb(18,18,18);
-  background-position:center top;
-  background-size:150%;
+  background: linear-gradient(rgba(18, 18, 18, 0.65), rgba(18, 18, 18, 0.65)),
+    url('/images/background/eerc_background.png') no-repeat fixed, rgb(18, 18, 18);
+  background-position: center top;
+  background-size: 150%;
 }
 
 .flagsAttribute {
